4532f06ee91c7c73bd1556737e80f1ff2b7ed997
Tino Maretic <pripravnik-tino.maretic@dump.hr>
2022-12-12 23:58:17 +0100

    Fixed Tables and added functions

--- SqlHomework.sql
+++ SqlHomework.sql
@@ -1,19 +1,3 @@
-CREATE TABLE Internships(
-	Id SERIAL PRIMARY KEY,
-	Year int NOT NULL,
-	DateOfBegining TIMESTAMP NOT NULL,
-	DateOfEnd TIMESTAMP NOT NULL,
-	Status varchar(10) CHECK (Status IN ('U pripremi', 'U tijeku', 'Završen')),
-	CONSTRAINT unique_date UNIQUE
-	(Year),
-	CONSTRAINT end_is_bigger CHECK
-	(DateOfEnd > DateOfBegining),
-	CONSTRAINT check_dateofbegining CHECK
-	(Year = DATE_PART('year', DateOfBegining)),
-	CONSTRAINT check_dateofend CHECK
-	(Year = DATE_PART('year', DateOfEnd))
-);
-
 CREATE TABLE Cities(
 	Id SERIAL PRIMARY KEY,
 	NAME varchar(15) NOT NULL UNIQUE
@@ -21,31 +5,52 @@ CREATE TABLE Cities(
 
 CREATE TABLE Members(
 	Id SERIAL PRIMARY KEY,
-	OIB varchar(11) NOT NULL UNIQUE,
+	OIB varchar(11) NOT NULL UNIQUE CHECK (LENGTH(OIB) = 11),
 	FirstName varchar(15) NOT NULL,
 	LastName varchar(15)NOT NULL,
-	Birth TIMESTAMP NOT NULL,
+	Birth DATE NOT NULL,
 	Gender varchar(1) CHECK (Gender IN ('M', 'F')),
-	CityId int REFERENCES Cities(id)
+	CityId int REFERENCES Cities(Id)
 );
 
-CREATE TABLE LeaderOfInternship(
-	InternshipId int REFERENCES Internships(Id),
-	LeaderId int REFERENCES Members(Id)
+CREATE TABLE Internships(
+	Id SERIAL PRIMARY KEY,
+	Year int NOT NULL,
+	DateOfBegining DATE NOT NULL,
+	DateOfEnd DATE NOT NULL,
+	Status varchar(10) CHECK (Status IN ('U pripremi', 'U tijeku', 'Završen')),
+	LeaderId int REFERENCES Members(Id),
+	CONSTRAINT unique_date UNIQUE
+	(Year),
+	CONSTRAINT end_is_bigger CHECK
+	(DateOfEnd > DateOfBegining),
+	CONSTRAINT check_dateofbegining CHECK
+	(Year = DATE_PART('year', DateOfBegining)),
+	CONSTRAINT check_date_of_end CHECK
+	(Year+1 = DATE_PART('year', DateOfEnd))
 );
+
+
 
 CREATE TABLE Fields (
 	Id SERIAL PRIMARY KEY,
-	Name varchar(15) NOT NULL UNIQUE CHECK (Name IN ('Programiranje', 'Dizajn', 'Marketing', 'Multimedia')),
+	Name varchar(15) NOT NULL CHECK (Name IN ('Programiranje', 'Dizajn', 'Marketing', 'Multimedia')),
+	LeaderId int REFERENCES Members(Id),
+	InternshipId int REFERENCES Internships(Id)
+);
+
+CREATE TABLE FieldMember(
+	FieldId int REFERENCES Fields(Id),
 	MemberId int REFERENCES Members(Id)
 );
 
+
 CREATE TABLE Interns(
 	Id SERIAL PRIMARY KEY,
-	OIB varchar(11) NOT NULL UNIQUE,
+	OIB varchar(11) NOT NULL UNIQUE CHECK (LENGth(OIB) = 11),
 	FirstName varchar(15) NOT NULL,
 	LastName varchar(15) NOT NULL,
-	Birth TIMESTAMP NOT NULL CHECK(DATE_PART('year', CURRENT_DATE) - DATE_PART('year', Birth) BETWEEN 16 AND 24),
+	Birth DATE NOT NULL CHECK(DATE_PART('year', CURRENT_DATE) - DATE_PART('year', Birth) BETWEEN 16 AND 24),
 	Gender varchar(1) CHECK (Gender IN ('M', 'F')),
 	CityId int REFERENCES Cities(Id)
 );
@@ -60,5 +65,40 @@ CREATE TABLE Homeworks(
 	Id SERIAL PRIMARY KEY,
 	Grade int NOT NULL CHECK (Grade BETWEEN 1 AND 5),
 	InternId int REFERENCES Interns(Id),
-	GraderId int REFERENCES Members(Id)
+	GraderId int REFERENCES Members(Id),
+	FieldId int REFERENCES Fields(id)
 );
+
+
+
+
+
+	
+	
+SELECT CONCAT(m.FirstName, m.LastName) AS ImeIPrezime FROM Members m
+WHERE m.CityId NOT IN
+(SELECT CityId FROM Cities WHERE Name LIKE 'Split');
+
+SELECT i.DateOfBegining AS DatumPocetka, i.DateOfEnd AS DatumKraja FROM Internships I
+ORDER BY i.DateOfBegining desc;
+
+SELECT i.FirstName, i.LastName FROM Interns i
+WHERE i.ID IN
+(SELECT ins.Id FROM InternStatus ins WHERE ins.InternId = i.Id IN
+ (SELECT f.Id FROM Fileds f WHERE f.Id = ins.FieldId IN
+  (SELECT ints.Id FROM Internships inst WHERE inst.year LIKE '2021' OR inst.yeat LIKE '2022' )));
+  
+SELECT COUNT(*) FROM Interns i
+where i.Gender LIKE 'F';
+
+SELECT COUNT(*) FROM Interns i
+WHERE i.Id IN
+(SELECT ins.Id FROM InternStatus ins WHERE ins.InternId = i.Id IN
+ (SELECT f.Id FROM Fields f WHERE f.Id = ins.Field ID IN(SELECT f.Id WHERE f.Status LIKE 'Izbačen');
+ 
+ 
+
+
+
+
+	
